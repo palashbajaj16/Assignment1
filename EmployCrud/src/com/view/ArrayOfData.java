196310955b3a7206f806c49d1e3149d6b459ccec
Palash Bajaj <86784927+palashbajaj16@users.noreply.github.com>
2021-07-24 00:05:47 +0530

    Optimize problem statement 3, Replace some code
    In this, I can optimize problem statement 3 I can change the arrayofdata.java file and search.java file

--- EmployCrud/src/com/view/ArrayOfData.java
+++ EmployCrud/src/com/view/ArrayOfData.java
@@ -1,33 +1,31 @@
 package com.view;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 
 public class ArrayOfData
 {
     public ArrayList ArrayOfDataEmp()
     {
+        String filepath = "D:\\Git Projects\\EmployCrud\\src\\com\\employees.txt";
         ArrayList<String> empList = new ArrayList<>();
         try
         {
-            FileInputStream fstream = new FileInputStream("D:\\Git Projects\\EmployCrud\\src\\com\\employees.txt");
-            DataInputStream in = new DataInputStream(fstream);
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            FileReader fr = new FileReader(filepath);
+            BufferedReader br = new BufferedReader(fr);
             String strLine;
             while ((strLine = br.readLine()) != null) {
-                empList.add(strLine);
+                if(strLine.length()!=0) {
+                    empList.add(strLine);
+                }
             }
-            in.close();
+            fr.close();
         }
-
         catch (Exception e)
         {
             System.err.println("Error: " + e.getMessage());
         }
-        empList.remove(0);
+        System.out.println(empList);
         return empList;
     }
 }
--- EmployCrud/src/com/view/Search.java
+++ EmployCrud/src/com/view/Search.java
@@ -1,25 +1,23 @@
 package com.view;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Search {
     Scanner sc = new Scanner(System.in);
-       void searchData() {
-           System.out.print("Enter Employ ID : ");
-           int quary = sc.nextInt();
-           ArrayOfData ard = new ArrayOfData();
-           ArrayList ar = ard.ArrayOfDataEmp();
-           for (int i=0;i<ar.size();i++)
-           {
-               String str = ar.get(i).toString();
-               String strid = str.substring(0,4);
-               int j = Integer.parseInt(strid);
-               if (j==quary)
-               {
-                   System.out.println(str);
-               }
-           }
+    ArrayOfData ard = new ArrayOfData();
+    void searchData() {
+        System.out.print("Enter Employ ID : ");
+        int quary = sc.nextInt();
+        for (Object val: ard.ArrayOfDataEmp())
+        {
+            String str = val.toString();
+            String strid = str.substring(0,4);
+            int j = Integer.parseInt(strid);
+            if (j==quary)
+            {
+                System.out.println(val);
+            }
+        }
 
        }
 }
